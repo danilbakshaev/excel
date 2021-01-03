7256aac86cf78c536eb6eac51f59302bbf0ae2b1
danilbakshaev <bakshaevdanil@gmail.com>
2021-01-03 14:42:29 +0500

    Настроил eslint для vscode

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "semi": "off",
+        "arrow-parens": "off",
+        "comma-dangle": "off",
+        "require-jsdoc": "off",
+        "linebreak-style": 0
+    },
+    "extends": ["eslint:recommended", "google"],
+    "parserOptions": {
+        "sourceType": "module"
+    }
+};
