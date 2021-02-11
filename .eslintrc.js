e427ffdb0a473f6daa84d73c78e89a92fb2d1f0d
danilbakshaev <bakshaevdanil@gmail.com>
2021-02-11 16:02:22 +0500

    update eslint for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": ["eslint:recommended", "google"],
-    "rules": {
-        "semi": "off",
-        "arrow-parens": "off",
-        "comma-dangle": "off",
-        "require-jsdoc": "off",
-        "linebreak-style": 0,
-        "operator-linebreak": "off"
-    },
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "parser": "babel-eslint"
-};
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    semi: 'off',
+    indent: 'off', // отключить, так как используется prettier
+    'arrow-parens': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off',
+    'linebreak-style': 0,
+    'operator-linebreak': 'off',
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,8 @@ module.exports = {
   singleQuote: true,
   semi: false,
   bracketSpacing: false,
+  ignoreChainWithDepth: 1,
+  printWidth: 80,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
 }
