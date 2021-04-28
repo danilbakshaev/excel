a6b5cbc21b66150af2438f2755b533b5a0115d8a
danilbakshaev <bakshaevdanil@gmail.com>
2021-04-28 20:55:27 +0500

    jest implemented

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,13 +1,13 @@
 import {Page} from '@core/Page'
+import {createStore} from '@core/store/createStore'
+import {rootReducer} from '@/redux/rootReducer'
+import {debounce, storage} from '@core/utils'
 import {Excel} from '@/components/excel/Excel'
 import {Header} from '@/components/header/Header'
 import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
-import {createStore} from '@core/createStore'
-import {rootReducer} from '@/redux/rootReducer'
-import {storage, debounce} from '@/core/utils'
-import {normalizeInitialState} from '../redux/initialState'
+import {normalizeInitialState} from '@/redux/initialState'
 
 function storageName(param) {
   return 'excel:' + param
